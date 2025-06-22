f7fbdb15a08a3e492cc85b9377284c111fb9f448
Abhijitam01 <abhijitamdubey31@gmail.com>
2025-06-22 15:51:03 +0530

    adding some config

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -3,13 +3,8 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 console.log("WebSocket server started on ws://localhost:8080");
 wss.on("connection", (ws) => {
-  console.log("Client connected");
-  ws.on("message", (message) => {
-    console.log(`Received message: ${message}`);
-    ws.send(`Echo: ${message}`);
+  const url = request.url ;
+  if(!url){
+    return;
+  }
   });
-  ws.on("close", () => {
-    console.log("Client disconnected");
-  });
-}
-);
