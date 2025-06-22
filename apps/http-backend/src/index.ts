fe92db06809e8fb357c2eff2b0a6b026d6377c9c
Abhijitam01 <abhijitamdubey31@gmail.com>
2025-06-23 04:17:38 +0530

    adding the zod achema and the packages

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,13 +1,33 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '@repo/backend-common/config';
+import { CreateUserSchema , SigninSchema , CreateRoomSchema } from '@repo/common/types'
 const app = express();
 
 app.post("/signup" , (req,res) =>{
 
+  const data = CreateUserSchema.safeParse(req.body);
+  if(!data.success){
+      res.json({
+      message : " Incorrect"
+    })
+    return
+  }
+
 })
 
 app.post("/signin" , (req , res) => {
+
+
+  const data = SigninSchema.safeParse(req.body);
+  if(!data.success){
+      res.json({
+      message : " Incorrect"
+    })
+    return
+  }
+
+
   const userId = 1;
   const token = jwt.sign({
     userId
@@ -16,6 +36,18 @@ app.post("/signin" , (req , res) => {
 })
 
 app.get("/room" , (req , res ) => {
+
+  const data = CreateRoomSchema.safeParse(req.body);
+  if(!data.success){
+      res.json({
+      message : " Incorrect"
+    })
+    return
+  }
+
+  res.json({
+    roomId : 133
+  })
   
 })
 app.listen(3000, () => {
